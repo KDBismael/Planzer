c4ecea6dd158a51b5a8f482527149cd6398aba48
Phillip Stemann <50196418+phillipstemann@users.noreply.github.com>
2022-02-15 20:59:47 +0100

    Initial commit

---
+++ components/task.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="mt-2 rounded-3 task-box">
+    <h6 class="description">Record yourself talking about the day</h6>
+    <div class="d-flex justify-content-between mt-3 align-items-center">
+      <p class="mb-0"># <span style="color: #fca3a9">youtube</span></p>
+      <div>
+        <label class="main">
+          <input type="checkbox" />
+          <span class="geekmark"></span>
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    task: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+};
+</script>
---
+++ pages/dashboard.vue
@@ -0,0 +1,291 @@
+<template>
+  <div class="container-fluid">
+    <div class="d-flex justify-content-between header">
+      <div class="d-flex align-items-center">
+        <img src="../assets/logo.png" class="logo" alt="Logo" />
+        <h5 class="fs-5 mb-0 ms-3 fw-bold">Company Name</h5>
+      </div>
+      <div class="d-flex align-items-center">
+        <img src="../assets/user.png" alt="" />
+        <h6 class="ms-2 mb-0">Phillip Stemann</h6>
+        <hexagon-icon size="1x" class="custom-class ms-3"></hexagon-icon>
+        <bell-icon size="1x" class="custom-class ms-3"></bell-icon>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-2">
+        <div class="d-flex flex-column align-items-center">
+          <VCalendar is-expanded :attributes="attrs" />
+          <div class="channel pt-4 mt-4">
+            <h4 class="fw-bold fs-5">Categories</h4>
+            <h5 class="fs-6 ms-3 mt-3">
+              <span class="allColor me-2">#</span> All
+            </h5>
+            <DropDown :data="workList" />
+            <DropDown :data="youtube" />
+            <DropDown :data="website" />
+            <div class="d-flex">
+              <button class="plus ms-2 d-flex justify-content-center align-items-center">+</button>
+              <h5 class="fs-6 ms-2">Manage Categories</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-7 task-row" id="task-row">
+        <div v-for="(column, index) in columns" :key="index" class="task-column">
+          <div class="day-contain">
+            <div class="d-flex justify-content-between">
+              <div>
+                <h5 class="fw-bold">{{ column.title }}</h5>
+                <h6 class="date">
+                  {{ month[column.date.getMonth()] }}
+                  {{ column.date.getUTCDate() }}
+                </h6>
+              </div>
+              <div class="progress yellow">
+                <span class="progress-left">
+                  <span class="progress-bar"></span>
+                </span>
+                <span class="progress-right">
+                  <span class="progress-bar"></span>
+                </span>
+                <div class="progress-value">33%</div>
+              </div>
+            </div>
+            <div class="d-flex justify-content-center mt-3">
+              <button class="add-task">Add a Task</button>
+            </div>
+            <draggable :id="column.date.getFullYear() + '-' +(column.date.getMonth() < 10 ? 0 : '')+(column.date.getMonth()+1) + '-' +column.date.getUTCDate()" @end="updateTaskDrop" :list="tasks.filter(filterDateTasks.bind(this, column.date.getFullYear() + '-' +column.date.getMonth() + '-' +column.date.getUTCDate()))" :animation="200" ghost-class="ghost-card" group="tasks.filter(filterDateTasks.bind(this, column.date.getFullYear() + '-' +column.date.getMonth() + '-' +column.date.getUTCDate()))">
+              <Task v-for="task in tasks.filter(filterDateTasks.bind(this, column.date.getFullYear() + '-' + column.date.getMonth() + '-' + column.date.getUTCDate()))" :key="task.id" :task="task" :id="task.id" />
+            </draggable>
+          </div>
+        </div>
+      </div>
+      <div class="col-3 d-flex">
+        <div class="schedule me-3">
+          <h5 class="fw-bold py-3 ps-3 border-bottom mb-0">Schedule</h5>
+          <div class="d-flex">
+            <div class="p-4 border-end">+</div>
+            <h4 class="p-4 mb-0">4 jan 2022</h4>
+          </div>
+          <div class="d-flex align-items-center" v-for="item in schedule" :key="item">
+            <div class="schedule-item text-center border-end py-3">
+              {{ item }}
+            </div>
+            <div class="border-bottom w-75"></div>
+          </div>
+        </div>
+        <div class="right-bar">
+          <div class="d-flex blue-gradient flex-column align-items-center justify-content-center">
+            <div class="p-3">
+              <target-icon size="1x" class="custom-class"></target-icon>
+            </div>
+            <div class="p-3">
+              <archive-icon size="1x" class="custom-class"></archive-icon>
+            </div>
+            <div class="p-3">
+              <save-icon size="1x" class="custom-class"></save-icon>
+            </div>
+            <div class="p-3">
+              <search-icon size="1x" class="custom-class"></search-icon>
+            </div>
+          </div>
+          <div class="d-flex red-gradient flex-column align-items-center justify-content-center mt-1">
+            <div class="p-3">
+              <plus-icon size="1x" class="custom-class"></plus-icon>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import draggable from "vuedraggable"
+import DropDown from "../components/dropdown.vue"
+import Task from "../components/task.vue"
+import { Calendar as VCalendar } from "v-calendar"
+import {
+  BellIcon,
+  HexagonIcon,
+  PlusIcon,
+  SearchIcon,
+  TargetIcon,
+  ArchiveIcon,
+  SaveIcon,
+} from "vue-feather-icons"
+
+export default {
+  auth: true,
+  components: {
+    draggable,
+    DropDown,
+    VCalendar,
+    Task,
+    BellIcon,
+    HexagonIcon,
+    PlusIcon,
+    SearchIcon,
+    TargetIcon,
+    ArchiveIcon,
+    SaveIcon,
+  },
+  computed: {
+    tasks() {
+      return this.$store.state.task.tasks
+    },
+  },
+  data() {
+    return {
+      attrs: [
+        {
+          highlight: {
+            start: { fillMode: "light" },
+            base: { fillMode: "light" },
+            end: { fillMode: "light" },
+          },
+          dates: { start: new Date(2022, 1, 4), end: new Date(2022, 1, 7) },
+        },
+      ],
+      month: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ],
+      weekday: [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ],
+      startDate: new Date(),
+      endDate: new Date(),
+      columns: [],
+      workList: {
+        title: "WORK",
+        lists: [
+          {
+            color: "red",
+            text: "clubhouse",
+          },
+          {
+            color: "#39cb5c",
+            text: "dkr-webinar",
+          },
+          {
+            color: "#f6ac6d",
+            text: "english",
+          },
+          {
+            color: "blue",
+            text: "job-posting",
+          },
+          {
+            color: "#a3a832",
+            text: "talks",
+          },
+          {
+            color: "blue",
+            text: "uxui_design",
+          },
+        ],
+      },
+      youtube: {
+        title: "YOUTUBE",
+        lists: [
+          {
+            color: "#39cb5c",
+            text: "All",
+          },
+        ],
+      },
+      website: {
+        title: "WEBSITE",
+        lists: [
+          {
+            color: "#a3a832",
+            text: "clubhouse",
+          },
+          {
+            color: "#f6ac6d",
+            text: "dkr-webinar",
+          },
+        ],
+      },
+      schedule: [
+        "1 AM",
+        "2 AM",
+        "3 AM",
+        "4 AM",
+        "5 AM",
+        "6 AM",
+        "7 AM",
+        "8 AM",
+        "9 AM",
+        "10 AM",
+        "11 AM",
+        "12 AM",
+      ],
+    }
+  },
+  destroyed() {
+    document.getElementById("task-row").removeEventListener("scroll", this.checkDates)
+  },
+  async beforeMount() {
+    this.startDate.setDate(this.startDate.getUTCDate() - 7)
+    this.endDate.setDate(this.endDate.getUTCDate() + 7)
+    for (var d = this.startDate; d <= this.endDate; d.setDate(d.getUTCDate() + 1)) {
+      this.columns.push({
+        title: this.weekday[d.getDay()],
+        date: new Date(d),
+      })
+    }
+    this.startDate.setDate(this.startDate.getUTCDate() - 15)
+    await this.$store.dispatch("task/getTasks", {
+      startDate: this.startDate,
+      endDate: this.endDate,
+    })
+  },
+  mounted() {
+    document.getElementById("task-row").addEventListener("scroll", this.checkDates)
+    document.getElementById("task-row").scrollLeft = 1950
+  },
+  methods: {
+    updateTaskDrop(event) {
+      this.$store.dispatch("task/update", {
+        task: this.tasks.find((task) => task.id == event.item.id),
+        newDate: event.to.id,
+      })
+    },
+    filterDateTasks(columnDate, task) {
+      var splitColumnDate = columnDate.split("-")
+      var newColumnDate = splitColumnDate[0] + "-" + (parseInt(splitColumnDate[1]) + 1 < 10 ? "0" : "") + (parseInt(splitColumnDate[1]) + 1) + "-" + (splitColumnDate[2] < 10 ? "0" : "") + splitColumnDate[2]
+
+      return task.date.substr(0, 10) == newColumnDate
+    },
+    async signOut() {
+      await this.$auth.logout()
+    },
+    checkDates(event) {
+      if (document.getElementById("task-row").scrollLeft == 0) {
+        console.log("Load 7 old days")
+      } else if (document.getElementById("task-row").scrollLeft + document.getElementById("task-row").offsetWidth == document.getElementById("task-row").scrollWidth) {
+        console.log("Load 7 new days")
+      }
+    },
+  },
+}
+</script>
