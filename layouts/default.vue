d48c682b9c44380c85777393cc46ae6136f9caad
Phillip Stemann <50196418+phillipstemann@users.noreply.github.com>
2022-03-17 17:32:29 +0100

    Fixed dropdown

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,21 +8,20 @@
       <div class="d-flex align-items-center">
         <img ref="user" src="../assets/user.png" alt="" />
         <h6 class="ms-2 mb-0">Phillip Stemann</h6>
-        <div class="dropdown ms-3">
+        <div class="dropdown ms-3 me-1">
           <a ref="dropdown" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
             <hexagon-icon size="1x"></hexagon-icon>
           </a>
 
           <ul class="dropdown-menu user-settings-dropdown" aria-labelledby="dropdownMenuLink">
-              <li><a class="dropdown-item" href="#"><UserIcon stroke="#4C79FF" class="me-2"/> Account settings</a></li>
-              <li><a class="dropdown-item" href="#"><HomeIcon stroke="#4C79FF" class="me-2"/>Workspace settings</a></li>
-              <li><a class="dropdown-item" href="#"><CreditCardIcon stroke="#4C79FF" class="me-2"/>Billing</a></li>
-              <li><a class="dropdown-item" href="#"><UserPlusIcon stroke="#4C79FF" class="me-2"/>Invite team member</a></li>
-              <li class="divider"><a class="dropdown-item" href="#"><CalendarIcon stroke="#4C79FF" class="me-2"/>Calendar settings</a></li>
-            <li><a @click="signOut()" class="dropdown-item" style="color:#FF5A5A" href="#"><LogOutIcon class="me-2"/>Sign out</a></li>
+              <li><a class="dropdown-item" href="#"><UserIcon size="1.5x" stroke="#4C79FF" class="me-2"/> Account settings</a></li>
+              <li><a class="dropdown-item" href="#"><HomeIcon size="1.5x" stroke="#4C79FF" class="me-2"/>Workspace settings</a></li>
+              <li><a class="dropdown-item" href="#"><CreditCardIcon size="1.5x" stroke="#4C79FF" class="me-2"/>Billing</a></li>
+              <li><a class="dropdown-item" href="#"><UserPlusIcon size="1.5x" stroke="#4C79FF" class="me-2"/>Invite team member</a></li>
+              <li class="divider"><a class="dropdown-item" href="#"><CalendarIcon size="1.5x" stroke="#4C79FF" class="me-2"/>Calendar settings</a></li>
+            <li><a @click="signOut()" class="dropdown-item" style="color:#FF5A5A" href="#"><LogOutIcon size="1.5x" class="me-2"/>Sign out</a></li>
           </ul>
         </div>
-        <bell-icon size="1x" class="ms-3"></bell-icon>
       </div>
     </div>
     <nuxt />
