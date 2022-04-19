961b91aa0fe19592fca2fd17b1eece9859050d6c
Phillip Stemann <stemannit@gmail.com>
2022-04-19 14:40:50 +0200

    Sign in on enter

--- pages/index.vue
+++ pages/index.vue
@@ -8,12 +8,12 @@
 
           <div class="mb-3">
             <label class="form-label">Email</label>
-            <input type="email" class="form-control" v-model="email">
+            <input type="email" @keyup.enter="login()" class="form-control" v-model="email">
           </div>
 
           <div class="mb-3">
             <label class="form-label">Password</label>
-            <input type="password" class="form-control" v-model="password">
+            <input type="password" @keyup.enter="login()" class="form-control" v-model="password">
           </div>
 
           <div class="sign-in-button">
